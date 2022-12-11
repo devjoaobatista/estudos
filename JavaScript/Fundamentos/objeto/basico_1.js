895025eab533b48d1c5a2c1ac097373bb358e19e
devjoaobatista <dev.joaobatista@gmail.com>
2022-12-11 17:23:15 -0300

    Novos treinos e conceitos

---
+++ JavaScript/Fundamentos/objeto/basico_1.js
@@ -0,0 +1,11 @@
+const numeros = [1, 2, 3]
+const produto = {
+    nome: "caneta",
+    preco: 9.99,
+};
+
+console.log(produto["nome"]);
+
+console.log(produto.nome);
+
+console.log(produto.preco);

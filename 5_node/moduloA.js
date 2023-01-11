09ed20407c4bbd0ed80cfd9e8fd71e563bddf17c
devjoaobatista <dev.joaobatista@gmail.com>
2023-01-11 11:36:50 -0300

    fouders organized, new exercises of node

---
+++ 5_node/moduloA.js
@@ -0,0 +1,5 @@
+//Exemplo de exportação
+this.ola = "fala Pessoal"
+exports.bemVindo = "Bem vindo ao node"
+
+module.exports.ateLogo = "Até o próximo exemplo"
---
+++ 5_node/moduloB.js
@@ -0,0 +1,7 @@
+//Exemplo de exportação
+module.exports = {
+    bomDia: "Bom dia!",
+    boaNoite() {
+        return "Boa noite!"
+    }
+}
